64f76ca739a14cee7c4e7e492b051b76987afee2
doruk.tekel <doruk.tekel@bilgeadam.com>
2024-06-10 17:40:42 +0300

    delete & signot functionality

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,4 +1,3 @@
-import { strict } from "assert";
 import { comparePassword, hashPassword } from "../helpers/password.js";
 import UserModel from "../models/userModel.js";
 import { errorHandler } from "../utils/errorHandler.js";
@@ -57,18 +56,18 @@ const google = async (req, res, next) => {
   try {
     const user = await UserModel.findOne({ email });
     if (user) {
-      const token = jwt.sign({ id: user_id }, process.env.JWT_SECRET_KEY, {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY, {
         expiresIn: "30d",
       });
       const { password: _, ...rest } = user._doc;
       res
-        .status(200)
-        .json(rest)
         .cookie("token", token, {
           httpOnly: true,
           maxAge: 30 * 24 * 60 * 60 * 1000,
           sameSite: "strict",
-        });
+        })
+        .status(200)
+        .json(rest);
     } else {
       const generatedPassword =
         Math.random().toString(36).slice(-8) +
@@ -105,4 +104,8 @@ const google = async (req, res, next) => {
   }
 };
 
-export { signup, signin, google };
+const signout = async (req, res) => {
+  res.clearCookie("token").status(200).json("Signout Success");
+};
+
+export { signup, signin, google, signout };
--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { testfunc, updateUser } from "../controllers/userController.js";
+import {
+  testfunc,
+  updateUser,
+  deleteUser,
+} from "../controllers/userController.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/", testfunc);
 router.post("/update/:id", verifyToken, updateUser);
+router.delete("/delete/:id", verifyToken, deleteUser);
 
 export default router;
